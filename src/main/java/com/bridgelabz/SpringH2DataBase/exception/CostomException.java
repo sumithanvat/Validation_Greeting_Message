34e0ad7645fd4d10ae60398c4a817855c1352e3a
Sumithanvat <skhanvat@gmail.com>
2023-07-21 15:38:35 +0530

    Added:CustomException is added

---
+++ src/main/java/com/bridgelabz/SpringH2DataBase/exception/CostomException.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.SpringH2DataBase.exception;
+
+public class CostomException extends RuntimeException{
+    public CostomException(String message){
+        super (message);
+    }
+}
--- src/main/java/com/bridgelabz/SpringH2DataBase/exception/ExceptionHandlerEx.java
+++ src/main/java/com/bridgelabz/SpringH2DataBase/exception/ExceptionHandlerEx.java
@@ -1,22 +1,32 @@
 package com.bridgelabz.SpringH2DataBase.exception;
 
 import com.bridgelabz.SpringH2DataBase.dto.ResponceDTO;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static java.util.stream.StreamSupport.stream;
 
 @ControllerAdvice
 public class ExceptionHandlerEx {
-    ExceptionHandler(MethodArgumentNotValidException.class)
-        public ResponseEntity<ResponceDTO>handelMethodArgumentNotValidException(MethodArgumentNotValidException exception){
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ResponceDTO> handelMethodArgumentNotValidException(MethodArgumentNotValidException exception){
+        List<ObjectError> errorList=exception.getBindingResult().getAllErrors();
+        List<String> errorMsg=errorList.stream().map(objErr -> objErr.getDefaultMessage()).collect(Collectors.toList());
+        ResponceDTO responceDTO =new ResponceDTO("Exception  While performing rest api",errorMsg);
+        return new ResponseEntity<>(responceDTO, HttpStatus.BAD_REQUEST);
+    }
+    @ExceptionHandler(CostomException.class)
+    public ResponseEntity<ResponceDTO> handleCustomException(CostomException exception){
+        ResponceDTO responceDTO=new ResponceDTO("Exception during performing Rest API",exception);
+        return new ResponseEntity<>(responceDTO,HttpStatus.BAD_REQUEST);
+    }
 
-        List<ObjectError> errorList.stream()
-
-        }
 
 }
